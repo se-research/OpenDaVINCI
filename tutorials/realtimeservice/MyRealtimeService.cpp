1eb666bc5648e92f7d550b9e682a984233712dd4
berger <christian.berger@gu.se>
2015-06-13 00:27:55 -0700

    * Tutorial sources

---
+++ tutorials/realtimeservice/MyRealtimeService.cpp
@@ -0,0 +1,45 @@
+/**
+ * OpenDaVINCI - Tutorial.
+ * Copyright (C) 2015 Christian Berger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <stdint.h>
+#include <iostream>
+#include <core/base/Thread.h>
+
+#include "MyRealtimeService.hpp"
+
+using namespace std;
+
+MyRealtimeService::MyRealtimeService(const enum PERIOD &period) :
+    core::base::RealtimeService(period) {}
+
+void MyRealtimeService::nextTimeSlice() {
+    cout << "This message is printed every 100 ms." << endl;        
+}
+
+int32_t main(int32_t argc, char **argv) {
+    MyRealtimeService rts(core::base::RealtimeService::HUNDREDMILLISECONDS);
+
+    rts.start();
+
+    const uint32_t ONE_SECOND = 1000 * 1000;
+    core::base::Thread::usleepFor(5 * ONE_SECOND);
+
+    rts.stop();
+}
+
