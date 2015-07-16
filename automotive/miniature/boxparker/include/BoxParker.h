c47d654295a328a4e9aea949b0de8f2ae3b24f98
Christian Berger <christian.berger@gu.se>
2015-07-16 13:46:03 -0700

    * BoxParker example

---
+++ automotive/miniature/boxparker/include/BoxParker.h
@@ -0,0 +1,77 @@
+/**
+ * boxparker - Sample application for realizing a box parking car.
+ * Copyright (C) 2012 - 2015 Christian Berger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef BOXPARKER_H_
+#define BOXPARKER_H_
+
+#include "core/base/TimeTriggeredConferenceClientModule.h"
+
+namespace automotive {
+    namespace miniature {
+
+        using namespace std;
+
+        /**
+         * This class is an example demonstrating how to park a simulated vehicle
+         * using OpenDaVINCI's driving dynamics simulation.
+         */
+        class BoxParker : public core::base::TimeTriggeredConferenceClientModule {
+            private:
+                /**
+                 * "Forbidden" copy constructor. Goal: The compiler should warn
+                 * already at compile time for unwanted bugs caused by any misuse
+                 * of the copy constructor.
+                 *
+                 * @param obj Reference to an object of this class.
+                 */
+                BoxParker(const BoxParker &/*obj*/);
+
+                /**
+                 * "Forbidden" assignment operator. Goal: The compiler should warn
+                 * already at compile time for unwanted bugs caused by any misuse
+                 * of the assignment operator.
+                 *
+                 * @param obj Reference to an object of this class.
+                 * @return Reference to this instance.
+                 */
+                BoxParker& operator=(const BoxParker &/*obj*/);
+
+            public:
+                /**
+                 * Constructor.
+                 *
+                 * @param argc Number of command line arguments.
+                 * @param argv Command line arguments.
+                 */
+                BoxParker(const int32_t &argc, char **argv);
+
+                virtual ~BoxParker();
+
+                core::base::ModuleState::MODULE_EXITCODE body();
+
+            private:
+                virtual void setUp();
+
+                virtual void tearDown();
+        };
+
+    } // miniature
+} // automotive
+
+#endif /*BOXPARKER_H_*/
---
+++ automotive/miniature/boxparker/src/BoxParker.cpp
@@ -0,0 +1,166 @@
+/**
+ * boxparker - Sample application for realizing a box parking car.
+ * Copyright (C) 2012 - 2015 Christian Berger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <cstdio>
+#include <cmath>
+
+#include "core/io/ContainerConference.h"
+#include "core/data/Container.h"
+#include "core/data/Constants.h"
+#include "core/data/environment/VehicleData.h"
+
+#include "GeneratedHeaders_CoreData.h"
+#include "GeneratedHeaders_AutomotiveData.h"
+
+#include "BoxParker.h"
+
+namespace automotive {
+    namespace miniature {
+
+        using namespace std;
+        using namespace core::base;
+        using namespace core::data;
+        using namespace core::data::environment;
+        using namespace coredata::control;
+        using namespace msv;
+
+        BoxParker::BoxParker(const int32_t &argc, char **argv) :
+	        TimeTriggeredConferenceClientModule(argc, argv, "BoxParker") {
+        }
+
+        BoxParker::~BoxParker() {}
+
+        void BoxParker::setUp() {
+	        // This method will be call automatically _before_ running body().
+        }
+
+        void BoxParker::tearDown() {
+	        // This method will be call automatically _after_ return from body().
+        }
+
+        // This method will do the main data processing job.
+        ModuleState::MODULE_EXITCODE BoxParker::body() {
+            double distanceOld = 0;
+            double absPathStart = 0;
+            double absPathEnd = 0;
+
+            int stageMoving = 0;
+            int stageMeasuring = 0;
+
+	        while (getModuleStateAndWaitForRemainingTimeInTimeslice() == ModuleState::RUNNING) {
+		        // 1. Get most recent vehicle data:
+		        Container containerVehicleData = getKeyValueDataStore().get(Container::VEHICLEDATA);
+		        VehicleData vd = containerVehicleData.getData<VehicleData> ();
+
+		        // 2. Get most recent sensor board data:
+		        Container containerSensorBoardData = getKeyValueDataStore().get(Container::USER_DATA_0);
+		        SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
+
+		        // Create vehicle control data.
+		        VehicleControl vc;
+
+                // Moving state machine.
+                if (stageMoving == 0) {
+                    // Go forward.
+                    vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(0);
+                }
+                if ((stageMoving > 0) && (stageMoving < 20)) {
+                    // Move slightly forward.
+                    vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(0);
+                    stageMoving++;
+                }
+                if ((stageMoving >= 20) && (stageMoving < 25)) {
+                    // Stop.
+                    vc.setSpeed(0);
+                    vc.setSteeringWheelAngle(0);
+                    stageMoving++;
+                }
+                if ((stageMoving >= 25) && (stageMoving < 80)) {
+                    // Backwards, steering wheel to the right.
+                    vc.setSpeed(-2);
+                    vc.setSteeringWheelAngle(25);
+                    stageMoving++;
+                }
+                if (stageMoving >= 80) {
+                    // Stop.
+                    vc.setSpeed(0);
+                    vc.setSteeringWheelAngle(0);
+
+                    stageMoving++;
+                }
+                if (stageMoving >= 150) {
+                    // End component.
+                    break;
+                }
+
+                // Measuring state machine.
+                switch (stageMeasuring) {
+                    case 0:
+                        {
+                            // Initialize measurement.
+                            distanceOld = sbd.getValueForKey_MapOfDistances(2);
+                            stageMeasuring++;
+                        }
+                    break;
+                    case 1:
+                        {
+                            // Checking for sequence +, -.
+                            if ((distanceOld > 0) && (sbd.getValueForKey_MapOfDistances(2) < 0)) {
+                                // Found sequence +, -.
+                                stageMeasuring = 2;
+                                absPathStart = vd.getAbsTraveledPath();
+                            }
+                            distanceOld = sbd.getValueForKey_MapOfDistances(2);
+                        }
+                    break;
+                    case 2:
+                        {
+                            // Checking for sequence -, +.
+                            if ((distanceOld < 0) && (sbd.getValueForKey_MapOfDistances(2) > 0)) {
+                                // Found sequence -, +.
+                                stageMeasuring = 1;
+                                absPathEnd = vd.getAbsTraveledPath();
+
+                                const double GAP_SIZE = (absPathEnd - absPathStart);
+
+                                cerr << "Size = " << GAP_SIZE << endl;
+
+                                if ((stageMoving < 1) && (GAP_SIZE > 3.5)) {
+                                    stageMoving = 1;
+                                }
+                            }
+                            distanceOld = sbd.getValueForKey_MapOfDistances(2);
+                        }
+                    break;
+                }
+
+		        // Create container for finally sending the data.
+		        Container c(Container::VEHICLECONTROL, vc);
+		        // Send container.
+		        getConference().send(c);
+	        }
+
+	        return ModuleState::OKAY;
+        }
+
+    } // miniature
+} // automotive
+
