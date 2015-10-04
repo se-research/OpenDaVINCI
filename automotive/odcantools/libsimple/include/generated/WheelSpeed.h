a4aca8c6550859ba43865aca37ba3d5899b73450
Christian Berger <christian.berger@gu.se>
2015-10-04 08:00:43 -0700

    * Started implementation sketch for code to be generated
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ automotive/odcantools/libsimple/include/generated/WheelSpeed.h
@@ -0,0 +1,6 @@
+/*
+ * This software is open source. Please see COPYING and AUTHORS for further information.
+ *
+ * This file is auto-generated. DO NOT CHANGE AS YOUR CHANGES MIGHT BE OVERWRITTEN!
+ */
+// Header file for: WheelSpeed
---
+++ automotive/odcantools/libsimple/testsuites/WheelSpeedTestSuite.h
@@ -0,0 +1,29 @@
+/*
+ * This software is open source. Please see COPYING and AUTHORS for further information.
+ *
+ * This file is auto-generated. DO NOT CHANGE AS YOUR CHANGES MIGHT BE OVERWRITTEN!
+ */
+// Test suite file for: WheelSpeed
+
+
+#ifndef SIMPLETESTSUITE_H_
+#define SIMPLETESTSUITE_H_
+
+#include "cxxtest/TestSuite.h"
+
+using namespace std;
+
+/**
+ * The actual testsuite starts here.
+ */
+class CANBridgeTest : public CxxTest::TestSuite {
+    public:
+        void testSample() {
+            TS_ASSERT(1 != 0);
+        }
+
+
+};
+
+#endif /*SIMPLETESTSUITE_H_*/
+
