bd7210cf15e5b44df1a8f82940e3942031937788
Christian Berger <christian.berger@gu.se>
2015-10-23 12:55:14 -0700

    * Correcting license to L-GPL 2.1
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- automotive/odcantools/libodcantools/src/MessageToCANDataStore.cpp
+++ automotive/odcantools/libodcantools/src/MessageToCANDataStore.cpp
@@ -1,5 +1,5 @@
 /**
- * odcanproxy - Tool wrapping a CAN interface.
+ * libodcantools - Library to wrap a CAN interface.
  * Copyright (C) 2015 Christian Berger
  *
  * This library is free software; you can redistribute it and/or
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
