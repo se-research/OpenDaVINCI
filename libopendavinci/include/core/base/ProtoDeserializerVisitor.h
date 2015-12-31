6b0a77d362e051cc3375094a0e336e818f43e3f4
Christian Berger <christian.berger@gu.se>
2015-12-31 11:34:15 +0100

    Missing include added

--- libopendavinci/include/core/base/ProtoDeserializerVisitor.h
+++ libopendavinci/include/core/base/ProtoDeserializerVisitor.h
@@ -25,14 +25,13 @@
 
 // platform.h must be included as first header file for definition of _WIN32_WINNT.
 #include "core/platform.h"
+#include "core/base/Serializer.h"
 #include "core/base/Deserializer.h"
 #include "core/base/ProtoSerializerVisitor.h"
 #include "core/base/Visitor.h"
 
 namespace core {
     namespace base {
-
-class Serializable;
 
         using namespace std;
 
