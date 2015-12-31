df321ee1f058bd0ab69d14a9909a2f0e24c5df77
Christian Berger <christian.berger@gu.se>
2015-12-31 12:39:12 +0100

    Missing include added

--- libopendavinci/include/core/wrapper/WIN32/WIN32UDPFactoryWorker.h
+++ libopendavinci/include/core/wrapper/WIN32/WIN32UDPFactoryWorker.h
@@ -20,6 +20,8 @@
 #ifndef OPENDAVINCI_CORE_WRAPPER_WIN32IMPL_WIN32UDPFACTORYWORKER_H_
 #define OPENDAVINCI_CORE_WRAPPER_WIN32IMPL_WIN32UDPFACTORYWORKER_H_
 
+#include <iostream>
+
 // core/platform.h must be included to setup platform-dependent header files and configurations.
 #include "core/platform.h"
 
