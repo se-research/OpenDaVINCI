a946e6197102f2c0ba5582041b0beca81547393b
Christian Berger <christian.berger@gu.se>
2016-02-08 11:53:57 -0800

    * Adjusted helloworld example
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- tutorials/helloworld/HelloWorldExample.h
+++ tutorials/helloworld/HelloWorldExample.h
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <core/base/module/TimeTriggeredConferenceClientModule.h>
+#include <opendavinci/core/base/module/TimeTriggeredConferenceClientModule.h>
 
 class HelloWorldExample : public core::base::module::TimeTriggeredConferenceClientModule {
     private:
