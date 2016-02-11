2d32ff51b09866f98547a86caab28f9eb78b1eb1
mrmurb <max@ig4.se>
2016-02-11 13:37:26 +0100

    Refractoring PythonInterp
    Specifying PythonInterp 2.7 in all locations.

--- odDataStructureGenerator/org.opendavinci.datamodel/test/odvd/reference/Test17_withCMake/libtest17/cmake.Modules/CheckCxxTestEnvironment.cmake
+++ odDataStructureGenerator/org.opendavinci.datamodel/test/odvd/reference/Test17_withCMake/libtest17/cmake.Modules/CheckCxxTestEnvironment.cmake
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-FIND_PACKAGE (PythonInterp)
+FIND_PACKAGE (PythonInterp 2.7)
 IF(PYTHONINTERP_FOUND)
     SET (CXXTEST_TESTGEN_ARGS --xunit-printer --have-eh)
     SET (CXXTEST_USE_PYTHON true)
