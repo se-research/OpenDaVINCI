815e72abed15bbbffd148c925106736ec13c9349
hang <young814young814@gmail.com>
2016-06-08 22:29:52 +0200

    Test the constraint on the number of shared memory segments

--- libopendavinci/testsuites/MemoryArrayTestSuite.h
+++ libopendavinci/testsuites/MemoryArrayTestSuite.h
@@ -46,92 +46,105 @@ class EigenExamplesTest : public CxxTest::TestSuite {
         void testEigenDataMappingTwoBytesUsingSharedPointCloud() {
             // Prepare constants to describe the PointCloud
             // stored in shared memory.
+            const int FRAME_NUMBER=5;
             const string NAME = "pointCloud";
             const string NAME2 = "Hang";
             const uint32_t SIZE_PER_COMPONENT = sizeof(float);
             const uint8_t NUMBER_OF_COMPONENTS_PER_POINT = 4; // How many components do we have per vector?
             const uint32_t LENGTH = 3; // How many points (i.e. vectors with (x,y,z,intensity)) are stored in the shared memory segment?
             const uint32_t SIZE = LENGTH * SIZE_PER_COMPONENT; // What is the total size of the shared memory?
-            const float sampleData[3][12]={
+            /*const float sampleData[3][12]={
                 {1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10.10,11.11,12.12},
                 {13.1,14.1,15.1,16.1,17.1,18.1,19.1,20.1,21.1,22.1,23.1,24.1},
-                {25.2,26.2,27.2,28.2,29.2,30.2,31.2,32.2,33.2,34.2,35.2,36.2}};
+                {25.2,26.2,27.2,28.2,29.2,30.2,31.2,32.2,33.2,34.2,35.2,36.2}};*/
+            float sampleData[FRAME_NUMBER][12];
+            for(int i=0;i<FRAME_NUMBER;i++){
+                for(int j=0;j<12;j++){
+                    sampleData[i][j]=i+(float)j*0.1;
+			        cout<<sampleData[i][j]<<" , ";
+		        }
+		        cout<<endl;
+	        }
+            Container toReceiver[FRAME_NUMBER];
+            Container fromSender[FRAME_NUMBER];
+            
             std::shared_ptr<SharedMemory> VelodyneSharedMemory;
             std::shared_ptr<SharedMemory> frameStore[3];
 
             try {
                 // This pointer would need to be created once from the sender at startup.
                 VelodyneSharedMemory=SharedMemoryFactory::createSharedMemory(NAME, SIZE);
-                for(int iii=0;iii<3;iii++){
+                for(int iii=0;iii<FRAME_NUMBER;iii++){
                     frameStore[iii]=SharedMemoryFactory::createSharedMemory(NAME2+to_string(iii), SIZE);
                 }
-                cout<<frameStore[0]->getSharedMemory()<<endl;
+                /*cout<<frameStore[0]->getSharedMemory()<<endl;
                 cout<<frameStore[1]->getSharedMemory()<<endl;
-                cout<<frameStore[2]->getSharedMemory()<<endl;
-                Container toReceiver;
+                cout<<frameStore[2]->getSharedMemory()<<endl;*/
+                
                 // Sender side.
+                cout << "Write to memory:" << endl;
                 {
-                    for(int iii=0;iii<3;iii++){
+                    for(int iii=0;iii<FRAME_NUMBER;iii++){
                         if (frameStore[iii]->isValid()) {
-                            {
-                                // Using a scoped lock to lock and automatically unlock a shared memory segment.
-                                odcore::base::Lock l(frameStore[iii]);
-                                float *velodyneRawData = static_cast<float*>(frameStore[iii]->getSharedMemory());
+                            // Using a scoped lock to lock and automatically unlock a shared memory segment.
+                            odcore::base::Lock l(frameStore[iii]);
+                            float *velodyneRawData = static_cast<float*>(frameStore[iii]->getSharedMemory());
 
-                                // Alignment of Velodyne data: (x0, y0, z0, intensity0), (x1, y1, z1, intensity1), ...
-                                // Add some example data.
-                                for(int jjj=0;jjj<12;jjj++){
-                                    velodyneRawData[jjj]=sampleData[iii][jjj];
-                                }
-
-                                cout << "Test" << endl;
-                                for(unsigned int i = 0; i < LENGTH * NUMBER_OF_COMPONENTS_PER_POINT; i++) {
-                                    cout << (float) velodyneRawData[i] << " ";
-                                }
-                                cout << endl;
+                            // Alignment of Velodyne data: (x0, y0, z0, intensity0), (x1, y1, z1, intensity1), ...
+                            // Add some example data.
+                            for(int jjj=0;jjj<12;jjj++){
+                                velodyneRawData[jjj]=sampleData[iii][jjj];
                             }
+
+                            //cout << "Test" << endl;
+                            for(unsigned int i = 0; i < LENGTH * NUMBER_OF_COMPONENTS_PER_POINT; i++) {
+                                cout << (float) velodyneRawData[i] << " , ";
+                            }
+                            cout << endl;
                         }
-                     }
-                     cout<<"Verify memory data"<<endl;
-                    if(VelodyneSharedMemory->isValid() && frameStore[1]->isValid()){
-                        Lock l1(VelodyneSharedMemory);
-                        Lock l2(frameStore[1]);
-                        memcpy(VelodyneSharedMemory->getSharedMemory(),frameStore[1]->getSharedMemory(),frameStore[1]->getSize());
-                        float *readFrame = static_cast<float*>(VelodyneSharedMemory->getSharedMemory());
-                        for(unsigned int i = 0; i < LENGTH * NUMBER_OF_COMPONENTS_PER_POINT; i++) {
-                                    cout << (float) readFrame[i] << " ";
-                                }
-                                cout << endl;
-                        
-                        SharedPointCloud spc;
-                        spc.setName(VelodyneSharedMemory->getName()); // Name of the shared memory segment with the data.
-                        spc.setSize(SIZE); // Size in raw bytes.
-                        spc.setWidth(LENGTH); // Number of points.
-                        spc.setHeight(1); // We have just a sequence of vectors.
-                        spc.setNumberOfComponentsPerPoint(NUMBER_OF_COMPONENTS_PER_POINT);
-                        spc.setComponentDataType(SharedPointCloud::FLOAT_T); // Data type per component.
-                        spc.setUserInfo(SharedPointCloud::XYZ_INTENSITY);
-                        
-                        toReceiver = Container(spc);
-                    } 
-                     
-                 }
+                    }
+                    cout<<"From memory to point cloud:"<<endl;
+                    for(int counter=0;counter<FRAME_NUMBER;counter++){
+                    //if(VelodyneSharedMemory->isValid() && frameStore[1]->isValid()){
+                        if(frameStore[counter]->isValid()){
+                            //Lock l1(VelodyneSharedMemory);
+                            Lock l2(frameStore[counter]);
+                            //memcpy(VelodyneSharedMemory->getSharedMemory(),frameStore[1]->getSharedMemory(),frameStore[1]->getSize());
+                            float *readFrame = static_cast<float*>(frameStore[counter]->getSharedMemory());
+                            for(unsigned int i = 0; i < LENGTH * NUMBER_OF_COMPONENTS_PER_POINT; i++) {
+                                cout << (float) readFrame[i] << " , ";
+                            }
+                            cout << endl;
+                            
+                            SharedPointCloud spc;
+                            spc.setName(frameStore[counter]->getName()); // Name of the shared memory segment with the data.
+                            spc.setSize(SIZE); // Size in raw bytes.
+                            spc.setWidth(LENGTH); // Number of points.
+                            spc.setHeight(1); // We have just a sequence of vectors.
+                            spc.setNumberOfComponentsPerPoint(NUMBER_OF_COMPONENTS_PER_POINT);
+                            spc.setComponentDataType(SharedPointCloud::FLOAT_T); // Data type per component.
+                            spc.setUserInfo(SharedPointCloud::XYZ_INTENSITY);
+                            
+                            toReceiver[counter] = Container(spc);
+                        } 
+                    }
+                }
                      // Receiver side.
                 {
                     // Let's assume we have received the container
                     // (would typically happen via the ContainerConference).
-                    Container fromSender = toReceiver;
+                    for(int frame=0;frame<FRAME_NUMBER;frame++){
+                        fromSender[frame] = toReceiver[frame];
 
-                    // Is it from the correct type?
-                    if (fromSender.getDataType() == SharedPointCloud::ID()) {
-                        SharedPointCloud senderSharedPointCloud = fromSender.getData<SharedPointCloud>();
-                        cout << senderSharedPointCloud.toString() << endl;
+                        // Is it from the correct type?
+                        if (fromSender[frame].getDataType() == SharedPointCloud::ID()) {
+                            SharedPointCloud senderSharedPointCloud = fromSender[frame].getData<SharedPointCloud>();
+                            //cout << senderSharedPointCloud.toString() << endl;
 
-                        // Attach ourselves to the same shared memory segment (would be needed just once).
-                        std::shared_ptr<SharedMemory> receiverSharedMemory(SharedMemoryFactory::attachToSharedMemory(senderSharedPointCloud.getName()));
+                            // Attach ourselves to the same shared memory segment (would be needed just once).
+                            std::shared_ptr<SharedMemory> receiverSharedMemory(SharedMemoryFactory::attachToSharedMemory(senderSharedPointCloud.getName()));
 
-                        if (receiverSharedMemory->isValid()) {
-                            {
+                            if (receiverSharedMemory->isValid()) {
                                 // Using a scoped lock to lock and automatically unlock a shared memory segment.
                                 odcore::base::Lock l(receiverSharedMemory);
                                 // We need to check (a) are we using the correct type (float)
@@ -144,38 +157,22 @@ class EigenExamplesTest : public CxxTest::TestSuite {
                                     // Get pointer to the beginning of the data.
                                     float *velodyneRawData = static_cast<float*>(receiverSharedMemory->getSharedMemory());
 
-                                    // Setup the Eigen mapping, where
-                                    // 4 == senderSharedPointCloud.getNumberOfComponentsPerPoint()
-                                    // that cannot be dynamic due to Eigen's design of being a
-                                    // compile-time library.
-                                    /*typedef Map<Matrix<float, Dynamic, Dynamic>, 0, InnerStride<4> > Slice;
-                                    Slice x((float*)velodyneRawData, senderSharedPointCloud.getWidth(), senderSharedPointCloud.getHeight());
-                                    Slice y((float*)velodyneRawData+1, senderSharedPointCloud.getWidth(), senderSharedPointCloud.getHeight());
-                                    Slice z((float*)velodyneRawData+2, senderSharedPointCloud.getWidth(), senderSharedPointCloud.getHeight());
-                                    Slice intensity((float*)velodyneRawData+3, senderSharedPointCloud.getWidth(), senderSharedPointCloud.getHeight());
-
-                                    cout << " ";
                                     for(unsigned int i = 0; i < senderSharedPointCloud.getWidth(); i++) {
-                                        cout << "x = " << (float)x(i, 0)
-                                             << ", y = " << (float)y(i, 0)
-                                             << ", z = " << (float)z(i, 0)
-                                             << ", intensity = " << (float)intensity(i, 0) << endl;
-                                    }*/
-                                    long startID=0;
-                                    for(unsigned int i = 0; i < senderSharedPointCloud.getWidth(); i++) {
-                                        cout << "x = " << velodyneRawData[startID]
+                                        /*cout << "x = " << velodyneRawData[startID]
                                              << ", y = " << velodyneRawData[startID+1]
                                              << ", z = " << velodyneRawData[startID+2]
                                              << ", intensity = " << velodyneRawData[startID+3] << endl;
-                                         startID=NUMBER_OF_COMPONENTS_PER_POINT*i;
+                                         startID=NUMBER_OF_COMPONENTS_PER_POINT*i;*/
+                                         cout<<velodyneRawData[i]<<" , ";
                                      }
+                                     cout<<endl;
                                 }
                             }
                         }
 
                     }
+                }
             }   
-        }
 
             catch(string &exception) {
                 cerr << "Shared memory could not created: " << exception << endl;
