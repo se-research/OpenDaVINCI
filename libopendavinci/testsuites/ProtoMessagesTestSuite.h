84cc95d409e6cf0583bd5a0f27a27692af0b0303
Christian Berger <christian.berger@gu.se>
2016-07-09 11:28:27 -0700

    * Separating test messages from core messages
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ libopendavinci/testsuites/ProtoMessagesTestSuite.h
@@ -0,0 +1,67 @@
+/**
+ * OpenDaVINCI - Portable middleware for distributed components.
+ * Copyright (C) 2008 - 2015 Christian Berger, Bernhard Rumpe
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef CORE_PROTOMESSAGESTESTSUITE_H_
+#define CORE_PROTOMESSAGESTESTSUITE_H_
+
+#include <cmath>                        // for fabs
+#include <iostream>                     // for stringstream, operator<<, etc
+#include <string>                       // for operator==, basic_string, etc
+
+#include "cxxtest/TestSuite.h"          // for TS_ASSERT, TestSuite
+
+#include "opendavinci/odcore/opendavinci.h"
+#include "opendavinci/odcore/base/ProtoDeserializerVisitor.h"
+#include "opendavinci/odcore/base/ProtoSerializerVisitor.h"
+
+#include "opendavincitestdata/generated/odcore/testdata/TestMessage1.h"
+
+using namespace std;
+using namespace odcore::base;
+using namespace odcore::testdata;
+
+class ProtoMessageTest : public CxxTest::TestSuite {
+    public:
+        void testSerializationDeserialization() {
+            TestMessage1 tm1;
+            tm1.setField1(12);
+
+            // Create a Proto serialization visitor.
+            ProtoSerializerVisitor protoSerializerVisitor;
+            tm1.accept(protoSerializerVisitor);
+
+            // Write the data to a stringstream.
+            stringstream out;
+            protoSerializerVisitor.getSerializedData(out);
+
+
+            // Create a Proto deserialization visitor.
+            ProtoDeserializerVisitor protoDeserializerVisitor;
+            protoDeserializerVisitor.deserializeDataFrom(out);
+
+            // Read back the data by using the visitor.
+            TestMessage1 tm2;
+            tm2.accept(protoDeserializerVisitor);
+
+            TS_ASSERT(tm1.getField1() == tm2.getField1());
+        }
+
+};
+
+#endif /*CORE_PROTOMESSAGESTESTSUITE_H_*/
