183888c9825189494131af170593d57c2e1c2d9b
Christian Berger <christian.berger@gu.se>
2016-07-27 02:39:28 -0700

    * Fixed Eigen warnings
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- libopendavinci/include/opendavinci/odcore/wrapper/Eigen.h
+++ libopendavinci/include/opendavinci/odcore/wrapper/Eigen.h
@@ -28,8 +28,12 @@
 # endif
 # pragma GCC diagnostic ignored "-Weffc++"
 # pragma GCC diagnostic ignored "-Wshadow"
-# pragma GCC diagnostic ignored "-Wignored-attributes"
-# pragma GCC diagnostic ignored "-Wmisleading-indentation"
+
+# if ((__GNUC__ * 100) + __GNUC_MINOR__) >= 600
+#  pragma GCC diagnostic ignored "-Wignored-attributes"
+#  pragma GCC diagnostic ignored "-Wmisleading-indentation"
+# endif
+
 #endif
     #include "opendavinci/Eigen/Core"
     #include "opendavinci/Eigen/Geometry"
