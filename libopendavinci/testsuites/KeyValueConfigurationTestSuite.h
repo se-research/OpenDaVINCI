2fac7104bca03e7cabcb0b7f81362a7d3ffe9028
Christian Berger <christian.berger@gu.se>
2016-10-07 20:19:28 +0200

    Revert "Merge pull request #87 from se-research/2016Q4.feature.OptionalConfigurationParameters"
    This reverts commit f292134ebbc5e679c1ef7f61a06e1475c2f71a7f, reversing
    changes made to 3d2eea7aadb68b08135e7d0891ec0a9cb6458e1b.

--- libopendavinci/testsuites/KeyValueConfigurationTestSuite.h
+++ libopendavinci/testsuites/KeyValueConfigurationTestSuite.h
@@ -47,62 +47,20 @@ class ConfigurationTest : public CxxTest::TestSuite {
             stringstream s2;
             kvc.writeTo(s2);
 
-            // test getValue<T>(...)
-            string value1 = "";
-            int32_t value2 = 0;
-            float value3 = 0;
-            double value4 = 0;
+            string key1 = "";
+            int32_t key2 = 0;
+            float key3 = 0;
+            double key4 = 0;
 
-            value1 = kvc.getValue<string>("Section1.key1");
-            value2 = kvc.getValue<int>("Section1.key2");
-            value3 = kvc.getValue<float>("Section1.key3");
-            value4 = kvc.getValue<double>("Section1.key4");
+            key1 = kvc.getValue<string>("Section1.key1");
+            key2 = kvc.getValue<int>("Section1.key2");
+            key3 = kvc.getValue<float>("Section1.key3");
+            key4 = kvc.getValue<double>("Section1.key4");
 
-            TS_ASSERT(value1 == "String1");
-            TS_ASSERT(value2 == 10);
-            TS_ASSERT_DELTA(value3, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4, 3.1415, 1e-3);
-
-            // test getOptionalValue<T>(...)
-            bool if1,if2,if3,if4;
-            if1=if2=if3=if4=false;
-            string value1o = "";
-            int32_t value2o = 0;
-            float value3o = 0;
-            double value4o = 0;
-
-            value1o = kvc.getOptionalValue<string>("Section1.key1", if1);
-            value2o = kvc.getOptionalValue<int>("Section1.key2", if2);
-            value3o = kvc.getOptionalValue<float>("Section1.key3", if3);
-            value4o = kvc.getOptionalValue<double>("Section1.key4", if4);
-
-            TS_ASSERT(if1);
-            TS_ASSERT(if2);
-            TS_ASSERT(if3);
-            TS_ASSERT(if4);
-            TS_ASSERT(value1o == "String1");
-            TS_ASSERT(value2o == 10);
-            TS_ASSERT_DELTA(value3o, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4o, 3.1415, 1e-3);
-
-            string value1nf = "xyz";
-            int32_t value2nf = 123;
-            float value3nf = 12.3;
-            double value4nf = 1.23;
-
-            value1nf = kvc.getOptionalValue<string>("Section1.key91", if1);
-            value2nf = kvc.getOptionalValue<int>("Section1.key92", if2);
-            value3nf = kvc.getOptionalValue<float>("Section1.key93", if3);
-            value4nf = kvc.getOptionalValue<double>("Section1.key94", if4);
-
-            TS_ASSERT(! if1);
-            TS_ASSERT(! if2);
-            TS_ASSERT(! if3);
-            TS_ASSERT(! if4);
-            TS_ASSERT(value1nf != "xyz");
-            TS_ASSERT(value2nf != 123);
-            TS_ASSERT(abs(value3nf-12.3) > 1e-3);
-            TS_ASSERT(abs(value4nf-1.23) > 1e-3);
+            TS_ASSERT(key1 == "String1");
+            TS_ASSERT(key2 == 10);
+            TS_ASSERT_DELTA(key3, 3.1415, 1e-3);
+            TS_ASSERT_DELTA(key4, 3.1415, 1e-3);
 
             stringstream s3;
             s3 << "section1.key1=String1" << endl
@@ -126,61 +84,20 @@ class ConfigurationTest : public CxxTest::TestSuite {
             stringstream s2;
             kvc.writeTo(s2);
 
-            string value1 = "";
-            int32_t value2 = 0;
-            float value3 = 0;
-            double value4 = 0;
+            string key1 = "";
+            int32_t key2 = 0;
+            float key3 = 0;
+            double key4 = 0;
 
-            value1 = kvc.getValue<string>("Section1.key1");
-            value2 = kvc.getValue<int>("Section1.key2");
-            value3 = kvc.getValue<float>("Section1.key3");
-            value4 = kvc.getValue<double>("Section1.key4");
+            key1 = kvc.getValue<string>("Section1.key1");
+            key2 = kvc.getValue<int>("Section1.key2");
+            key3 = kvc.getValue<float>("Section1.key3");
+            key4 = kvc.getValue<double>("Section1.key4");
 
-            TS_ASSERT(value1 == "String1");
-            TS_ASSERT(value2 == 10);
-            TS_ASSERT_DELTA(value3, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4, 3.1415, 1e-3);
-
-            // test getOptionalValue<T>(...)
-            bool if1,if2,if3,if4;
-            if1=if2=if3=if4=false;
-            string value1o = "";
-            int32_t value2o = 0;
-            float value3o = 0;
-            double value4o = 0;
-
-            value1o = kvc.getOptionalValue<string>("Section1.key1", if1);
-            value2o = kvc.getOptionalValue<int>("Section1.key2", if2);
-            value3o = kvc.getOptionalValue<float>("Section1.key3", if3);
-            value4o = kvc.getOptionalValue<double>("Section1.key4", if4);
-
-            TS_ASSERT(if1);
-            TS_ASSERT(if2);
-            TS_ASSERT(if3);
-            TS_ASSERT(if4);
-            TS_ASSERT(value1o == "String1");
-            TS_ASSERT(value2o == 10);
-            TS_ASSERT_DELTA(value3o, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4o, 3.1415, 1e-3);
-
-            string value1nf = "xyz";
-            int32_t value2nf = 123;
-            float value3nf = 12.3;
-            double value4nf = 1.23;
-
-            value1nf = kvc.getOptionalValue<string>("Section1.key91", if1);
-            value2nf = kvc.getOptionalValue<int>("Section1.key92", if2);
-            value3nf = kvc.getOptionalValue<float>("Section1.key93", if3);
-            value4nf = kvc.getOptionalValue<double>("Section1.key94", if4);
-
-            TS_ASSERT(! if1);
-            TS_ASSERT(! if2);
-            TS_ASSERT(! if3);
-            TS_ASSERT(! if4);
-            TS_ASSERT(value1nf != "xyz");
-            TS_ASSERT(value2nf != 123);
-            TS_ASSERT(abs(value3nf-12.3) > 1e-3);
-            TS_ASSERT(abs(value4nf-1.23) > 1e-3);
+            TS_ASSERT(key1 == "String1");
+            TS_ASSERT(key2 == 10);
+            TS_ASSERT_DELTA(key3, 3.1415, 1e-3);
+            TS_ASSERT_DELTA(key4, 3.1415, 1e-3);
 
             stringstream s3;
             s3 << "section1.key1=String1" << endl
@@ -205,61 +122,20 @@ class ConfigurationTest : public CxxTest::TestSuite {
             stringstream s2;
             kvc.writeTo(s2);
 
-            string value1 = "";
-            int32_t value2 = 0;
-            float value3 = 0;
-            double value4 = 0;
+            string key1 = "";
+            int32_t key2 = 0;
+            float key3 = 0;
+            double key4 = 0;
 
-            value1 = kvc.getValue<string>("Section1.key1");
-            value2 = kvc.getValue<int>("Section1.key2");
-            value3 = kvc.getValue<float>("Section1.key3");
-            value4 = kvc.getValue<double>("Section1.key4");
+            key1 = kvc.getValue<string>("Section1.key1");
+            key2 = kvc.getValue<int>("Section1.key2");
+            key3 = kvc.getValue<float>("Section1.key3");
+            key4 = kvc.getValue<double>("Section1.key4");
 
-            TS_ASSERT(value1 == "String1");
-            TS_ASSERT(value2 == 10);
-            TS_ASSERT_DELTA(value3, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4, 3.1415, 1e-3);
-
-            // test getOptionalValue<T>(...)
-            bool if1,if2,if3,if4;
-            if1=if2=if3=if4=false;
-            string value1o = "";
-            int32_t value2o = 0;
-            float value3o = 0;
-            double value4o = 0;
-
-            value1o = kvc.getOptionalValue<string>("Section1.key1", if1);
-            value2o = kvc.getOptionalValue<int>("Section1.key2", if2);
-            value3o = kvc.getOptionalValue<float>("Section1.key3", if3);
-            value4o = kvc.getOptionalValue<double>("Section1.key4", if4);
-
-            TS_ASSERT(if1);
-            TS_ASSERT(if2);
-            TS_ASSERT(if3);
-            TS_ASSERT(if4);
-            TS_ASSERT(value1o == "String1");
-            TS_ASSERT(value2o == 10);
-            TS_ASSERT_DELTA(value3o, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4o, 3.1415, 1e-3);
-
-            string value1nf = "xyz";
-            int32_t value2nf = 123;
-            float value3nf = 12.3;
-            double value4nf = 1.23;
-
-            value1nf = kvc.getOptionalValue<string>("Section1.key91", if1);
-            value2nf = kvc.getOptionalValue<int>("Section1.key92", if2);
-            value3nf = kvc.getOptionalValue<float>("Section1.key93", if3);
-            value4nf = kvc.getOptionalValue<double>("Section1.key94", if4);
-
-            TS_ASSERT(! if1);
-            TS_ASSERT(! if2);
-            TS_ASSERT(! if3);
-            TS_ASSERT(! if4);
-            TS_ASSERT(value1nf != "xyz");
-            TS_ASSERT(value2nf != 123);
-            TS_ASSERT(abs(value3nf-12.3) > 1e-3);
-            TS_ASSERT(abs(value4nf-1.23) > 1e-3);
+            TS_ASSERT(key1 == "String1");
+            TS_ASSERT(key2 == 10);
+            TS_ASSERT_DELTA(key3, 3.1415, 1e-3);
+            TS_ASSERT_DELTA(key4, 3.1415, 1e-3);
 
             stringstream s3;
             s3 << "section1.key1=String1" << endl
@@ -281,52 +157,26 @@ class ConfigurationTest : public CxxTest::TestSuite {
             stringstream s2;
             kvc.writeTo(s2);
 
-            string value1 = "";
-            int32_t value2 = 0;
-            float value3 = 0;
-            double value4 = 0;
+            string key1 = "";
+            int32_t key2 = 0;
+            float key3 = 0;
+            double key4 = 0;
 
-            bool value1NotFound = false;
+            bool key1NotFound = false;
             try {
-                value1 = kvc.getValue<string>("Section1.key1");
+                key1 = kvc.getValue<string>("Section1.key1");
             } catch (ValueForKeyNotFoundException &vfknfe) {
                 TS_ASSERT(vfknfe.getMessage() == "Value for key 'Section1.key1' not found.");
-                value1NotFound = true;
+                key1NotFound = true;
             }
-            value2 = kvc.getValue<int>("Section1.key2");
-            value3 = kvc.getValue<float>("Section1.key3");
-            value4 = kvc.getValue<double>("Section3.key4");
+            key2 = kvc.getValue<int>("Section1.key2");
+            key3 = kvc.getValue<float>("Section1.key3");
+            key4 = kvc.getValue<double>("Section3.key4");
 
-            TS_ASSERT(value1NotFound);
-            TS_ASSERT(value2 == 10);
-            TS_ASSERT_DELTA(value3, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4, 3.1415, 1e-3);
-
-            // test getOptionalValue<T>(...)
-            // the retrieval of parameters not affected by comments has been tested in the previous tests and it is now skipped
-            bool if1,if3;
-            if1=true;
-            if3=false;
-            string value1o = "";
-            float value3o = 0;
-
-            value1o = kvc.getOptionalValue<string>("Section1.key1", if1);
-            value3o = kvc.getOptionalValue<float>("Section1.key3", if3);
-
-            TS_ASSERT(! if1);
-            TS_ASSERT(if3);
-            TS_ASSERT_DELTA(value3o, 3.1415, 1e-3);
-
-            string value1nf = "xyz";
-            float value3nf = 12.3;
-
-            value1nf = kvc.getOptionalValue<string>("Section1.key91", if1);
-            value3nf = kvc.getOptionalValue<float>("Section1.key93", if3);
-
-            TS_ASSERT(value1nf != "xyz");
-            TS_ASSERT(abs(value3nf-12.3) > 1e-3);
-            TS_ASSERT(! if1);
-            TS_ASSERT(! if3);
+            TS_ASSERT(key1NotFound);
+            TS_ASSERT(key2 == 10);
+            TS_ASSERT_DELTA(key3, 3.1415, 1e-3);
+            TS_ASSERT_DELTA(key4, 3.1415, 1e-3);
 
             stringstream s3;
             s3 << "section1.key2=10" << endl
@@ -345,49 +195,26 @@ class ConfigurationTest : public CxxTest::TestSuite {
             KeyValueConfiguration kvc;
             kvc.readFrom(s);
 
-            string value1 = "";
-            int32_t value2 = 0;
-            float value3 = 0;
-            double value4 = 0;
+            string key1 = "";
+            int32_t key2 = 0;
+            float key3 = 0;
+            double key4 = 0;
 
-            bool value1NotFound = false;
+            bool key1NotFound = false;
             try {
-                value1 = kvc.getValue<string>("SeCtIoN1.key1");
+                key1 = kvc.getValue<string>("SeCtIoN1.key1");
             } catch (ValueForKeyNotFoundException &vfknfe) {
                 TS_ASSERT(vfknfe.getMessage() == "Value for key 'SeCtIoN1.key1' not found.");
-                value1NotFound = true;
+                key1NotFound = true;
             }
-            value2 = kvc.getValue<int>("Section1.key2");
-            value3 = kvc.getValue<float>("SeCtIoN1.KEy3");
-            value4 = kvc.getValue<double>("SectION3.kEY4");
+            key2 = kvc.getValue<int>("Section1.kEY2");
+            key3 = kvc.getValue<float>("SeCtIoN1.key3");
+            key4 = kvc.getValue<double>("Section3.KeY4");
 
-            TS_ASSERT(value1NotFound);
-            TS_ASSERT(value2 == 10);
-            TS_ASSERT_DELTA(value3, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4, 3.1415, 1e-3);
-            
-            // test getOptionalValue<T>(...)
-            bool if1,if2,if3,if4;
-            if1=true;
-            if2=if3=if4=false;
-            string value1nf = "";
-            int32_t value2o = 0;
-            float value3o = 0;
-            double value4o = 0;
-
-            value1nf = kvc.getOptionalValue<string>("sEcTiON1.kEY1", if1);
-            value2o = kvc.getOptionalValue<int>("sEcTiON1.KEy2", if2);
-            value3o = kvc.getOptionalValue<float>("SecTIoN1.keY3", if3);
-            value4o = kvc.getOptionalValue<double>("SeCTion3.kEy4", if4);
-
-            TS_ASSERT(! if1);
-            TS_ASSERT(if2);
-            TS_ASSERT(if3);
-            TS_ASSERT(if4);
-            TS_ASSERT(value1nf != "String1");
-            TS_ASSERT(value2o == 10);
-            TS_ASSERT_DELTA(value3o, 3.1415, 1e-3);
-            TS_ASSERT_DELTA(value4o, 3.1415, 1e-3);
+            TS_ASSERT(key1NotFound);
+            TS_ASSERT(key2 == 10);
+            TS_ASSERT_DELTA(key3, 3.1415, 1e-3);
+            TS_ASSERT_DELTA(key4, 3.1415, 1e-3);
         }
 
         void testReadCommentedDataSubset() {
@@ -401,26 +228,11 @@ class ConfigurationTest : public CxxTest::TestSuite {
             kvcOriginal.readFrom(s);
 
             KeyValueConfiguration kvc = kvcOriginal.getSubsetForSection("Section3");
-            double value4 = 0;
+            double key4 = 0;
 
-            value4 = kvc.getValue<double>("Section3.key4");
+            key4 = kvc.getValue<double>("Section3.KeY4");
 
-            TS_ASSERT_DELTA(value4, 3.1415, 1e-3);
-            
-            // test getOptionalValue<T>(...)
-            bool if4,if5;
-            if4=false;
-            if5=true;
-            double value4o = 0;
-            double value5nf = 1.618;
-
-            value4o = kvc.getOptionalValue<double>("sEcTiON3.Key4", if4);
-            value5nf = kvc.getOptionalValue<double>("sEcTiOn3.kEY5", if5);
-
-            TS_ASSERT(if4);
-            TS_ASSERT_DELTA(value4o, 3.1415, 1e-3);
-            TS_ASSERT(! if5);
-            TS_ASSERT(abs(value5nf-1.618) > 1e-3);
+            TS_ASSERT_DELTA(key4, 3.1415, 1e-3);
         }
 };
 
