25a71aa95244647eb1c62453701fc6cb53d1bc06
Crispin Kirchner <crispin.kirchner@rwth-aachen.de>
2016-11-14 19:45:47 +0100

    made functional again

--- libopendavinci/include/opendavinci/odcore/base/Arguments.h
+++ libopendavinci/include/opendavinci/odcore/base/Arguments.h
@@ -1,49 +1,39 @@
-#include <cstring>
 #include <memory>
 #include <vector>
 
-/**
- * Helper class to easily get argc and argv values from an initializer list.
- *
- * The template parameter is the type to be used for `argc`.
- */
-class Arguments {
-    public:
-    Arguments(std::initializer_list<std::string> argList)
-        : argvPtr_(new char *[argList.size()])
-        , argc(argList.size())
-        , argv(argvPtr_.get())  
-    {
-        // copy the initializer list into this object
-        int32_t i = 0;
-        for (const auto &arg : argList) {
-            const int32_t lengthIncludingTerminatingZero = arg.length() + 1;
+namespace odcore {
+    namespace base {
 
-            // allocate space
-            argvValues_.emplace_back(new char[lengthIncludingTerminatingZero]);
+        /**
+         * Helper class to easily get argc and argv values from an initializer list.
+         *
+         * Usage:
+         *
+         *     Arguments args{"foo", "bar"};
+         *     Module mod1(args.argc, args.argv);
+         *
+         * There is no copy-, but a move-constructor / assignment.
+         */
+        class Arguments {
+            public:
+            Arguments(const std::initializer_list<std::string> & argList);
 
-            char * cstring = argvValues_.rbegin()->get();
+            private:
+            /**
+             * Vector containing the actual, allocated arguments
+             */
+            std::vector<std::unique_ptr<char[]>> argvValues_;
 
-            // copy content
-            std::strncpy(cstring, arg.c_str(), lengthIncludingTerminatingZero);
+            /**
+             * C-style array to return when calling argv(). Contains pointers to the
+             * entries in argvValues_. Automatically deallocated on Arguments object
+             * destruction.
+             */
+            std::unique_ptr<char* []> argvPtr_;
 
-            argvPtr_[i++] = cstring;
-        }
+            public:
+            const int32_t argc;
+            char ** const argv;
+        };
     }
-
-    const int32_t argc;
-    char ** const argv;
-
-    private:
-    /**
-     * Vector containing the actual, allocated arguments
-     */
-    std::vector<std::unique_ptr<char[]>> argvValues_;
-
-    /**
-     * C-style array to return when calling argv(). Contains pointers to the
-     * entries in argvValues_. Automatically deallocated on Arguments object
-     * destruction.
-     */
-    std::unique_ptr<char *[]> argvPtr_;
-};
+}
