2564929e2c17934cfa2d4872c6e37246e2e9d8af
Christian Berger <christian.berger@gu.se>
2017-01-03 16:59:25 +0100

    * Started new implementation for Player
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ libopendavinci/include/opendavinci/odtools/player/Player2.h
@@ -0,0 +1,102 @@
+/**
+ * OpenDaVINCI - Portable middleware for distributed components.
+ * Copyright (C) 2017 Christian Berger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef OPENDAVINCI_TOOLS_PLAYER_PLAYER2_H_
+#define OPENDAVINCI_TOOLS_PLAYER_PLAYER2_H_
+
+#include <map>
+
+#include <opendavinci/odcore/opendavinci.h>
+#include <opendavinci/odcore/data/Container.h>
+#include <opendavinci/odcore/io/URL.h>
+
+namespace odtools {
+    namespace player {
+
+        using namespace std;
+
+        /**
+         * This class can be used to read previously recorded
+         * data from a given URL.
+         */
+        class OPENDAVINCI_API Player2 {
+            private:
+                /**
+                 * "Forbidden" copy constructor. Goal: The compiler should warn
+                 * already at compile time for unwanted bugs caused by any misuse
+                 * of the copy constructor.
+                 *
+                 * @param obj Reference to an object of this class.
+                 */
+                Player2(const Player2 &/*obj*/) = delete;
+
+                /**
+                 * "Forbidden" assignment operator. Goal: The compiler should warn
+                 * already at compile time for unwanted bugs caused by any misuse
+                 * of the assignment operator.
+                 *
+                 * @param obj Reference to an object of this class.
+                 * @return Reference to this instance.
+                 */
+                Player2& operator=(const Player2 &/*obj*/) = delete;
+
+            public:
+                /**
+                 * Constructor.
+                 *
+                 * @param url Resource to play.
+                 */
+                Player2(const odcore::io::URL &url);
+
+                virtual ~Player2();
+
+                /**
+                 * This method returns the next container to be replayed.
+                 *
+                 * @return Next container to be replayed.
+                 */
+//                const odcore::data::Container& getNextContainerToBeSent();
+                odcore::data::Container getNextContainerToBeSent();
+
+                /**
+                 * This method returns the delay to be waited before the next container should be delivered.
+                 *
+                 * @return delay to the next container in real time microseconds (us).
+                 */
+                uint32_t getDelay() const;
+
+                /**
+                 * This method returns true if there is more data to replay.
+                 *
+                 * @return true if there is more data to replay.
+                 */
+                bool hasMoreData() const;
+
+            private:
+                void fillCache(const string &resource);
+
+            private:
+                multimap<int64_t, odcore::data::Container> m_cache;
+                multimap<int64_t, odcore::data::Container>::const_iterator m_current;
+        };
+
+    } // player
+} // tools
+
+#endif /*OPENDAVINCI_TOOLS_PLAYER_PLAYER2_H_*/
---
+++ odtools/odplayer/testsuites/Player2TestSuite.h
@@ -0,0 +1,93 @@
+/**
+ * odplayer - Tool for playing back recorded data
+ * Copyright (C) 2017 Christian Berger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef PLAYER2TESTSUITE_H_
+#define PLAYER2TESTSUITE_H_
+
+#include "cxxtest/TestSuite.h"
+
+#include <fstream>
+#include <iostream>
+
+#include <opendavinci/odcore/data/Container.h>
+#include <opendavinci/odcore/data/TimeStamp.h>
+#include <opendavinci/odcore/io/URL.h>
+#include <opendavinci/odtools/player/Player2.h>
+
+using namespace std;
+using namespace odcore::base;
+using namespace odcore::data;
+using namespace odcore::io;
+using namespace odtools::player;
+
+class PlayerModule2Test : public CxxTest::TestSuite {
+    public:
+        void testRealtimeReplayNoRewind() {
+            // Prepare record file.
+            fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
+
+            // Write data with non-monotonic order.
+            TimeStamp ts1(4, 5);
+            TimeStamp ts2(2, 3);
+            TimeStamp ts3(0, 1);
+            TimeStamp ts4(1, 2);
+            TimeStamp ts5(3, 4);
+
+            Container c1(ts1);
+            c1.setSampleTimeStamp(ts1);
+            fout << c1;
+
+            Container c2(ts2);
+            c2.setSampleTimeStamp(ts2);
+            fout << c2;
+
+            Container c3(ts3);
+            c3.setSampleTimeStamp(ts3);
+            fout << c3;
+
+            Container c4(ts4);
+            c4.setSampleTimeStamp(ts4);
+            fout << c4;
+
+            Container c5(ts5);
+            c5.setSampleTimeStamp(ts5);
+            fout << c5;
+
+            fout.flush();
+            fout.close();
+
+            const URL u("file://PlayerModule2Test.rec");
+
+            // Create Player2 instance.
+            Player2 p2(u);
+
+            // Check whether Player stores the entries in correct time order.
+            int64_t counter = 0;
+            while (p2.hasMoreData()) {
+                Container c = p2.getNextContainerToBeSent();
+                TS_ASSERT((counter * 1000 * 1000 + (counter + 1)) == c.getSampleTimeStamp().toMicroseconds());
+                counter++;
+            }
+
+            UNLINK("PlayerModuleTest2.rec");
+        }
+
+};
+
+#endif /*PLAYER2TESTSUITE_H_*/
