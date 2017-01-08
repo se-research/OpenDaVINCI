6294e4140553375665e3fcb99987d77110f0d161
Christian Berger <christian.berger@gu.se>
2017-01-08 17:19:56 +0100

    synchronizing parallel reading
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- odtools/odplayer/testsuites/Player2TestSuite.h
+++ odtools/odplayer/testsuites/Player2TestSuite.h
@@ -535,7 +535,7 @@ class PlayerModule2Test : public CxxTest::TestSuite {
             UNLINK("PlayerModule2Test2.rec");
         }
 
-        void notestLargeFile() {
+        void testLargeFile() {
             cout << __FILE__ << " " << __LINE__ << endl;
             const URL u("file://test.rec");
 
