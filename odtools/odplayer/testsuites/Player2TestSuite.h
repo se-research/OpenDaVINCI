a8a1ca4e74666437fb88f6298e7f0c16a9c341d2
Christian Berger <christian.berger@gu.se>
2017-02-20 20:29:38 +0100

    * Added test case for SharedMemory
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- odtools/odplayer/testsuites/Player2TestSuite.h
+++ odtools/odplayer/testsuites/Player2TestSuite.h
@@ -33,6 +33,7 @@
 #include <opendavinci/odcore/data/TimeStamp.h>
 #include <opendavinci/odcore/io/URL.h>
 #include <opendavinci/odtools/player/Player2.h>
+#include <opendavinci/GeneratedHeaders_OpenDaVINCI.h>
 
 using namespace std;
 using namespace odcore::base;
@@ -44,6 +45,8 @@ class PlayerModule2Test : public CxxTest::TestSuite {
     public:
         void testEmptyFileAndException() {
             cout << __FILE__ << " " << __LINE__ << endl;
+            UNLINK("PlayerModule2TestEmpty.rec");
+
             // Prepare record file.
             fstream fout("PlayerModule2TestEmpty.rec", ios::out | ios::binary | ios::trunc);
             fout.flush();
@@ -74,6 +77,9 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
         void testCorrectMonotonicTemporalOrderAndExceptionFromReverseOrder() {
             cout << __FILE__ << " " << __LINE__ << endl;
+            UNLINK("PlayerModule2Test.rec");
+            UNLINK("PlayerModule2Test.rec.mem");
+
             // Prepare record file.
             fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
 
@@ -141,11 +147,14 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
             TS_ASSERT(exceptionCaught);
 
-            UNLINK("PlayerModuleTest2.rec");
+            UNLINK("PlayerModule2Test.rec");
         }
 
         void testCorrectMonotonicTemporalOrderAndExceptionFromReverseOrderRealtimeReplay() {
             cout << __FILE__ << " " << __LINE__ << endl;
+            UNLINK("PlayerModule2Test.rec");
+            UNLINK("PlayerModule2Test.rec.mem");
+
             // Prepare record file.
             fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
 
@@ -212,11 +221,14 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
             TS_ASSERT(exceptionCaught);
 
-            UNLINK("PlayerModuleTest2.rec");
+            UNLINK("PlayerModule2Test.rec");
         }
 
         void testCorrectMonotonicTemporalOrderAndExceptionFromReverseOrderWithDuplicateEntries() {
             cout << __FILE__ << " " << __LINE__ << endl;
+            UNLINK("PlayerModule2Test.rec");
+            UNLINK("PlayerModule2Test.rec.mem");
+
             // Prepare record file.
             fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
 
@@ -300,11 +312,14 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
             TS_ASSERT(exceptionCaught);
 
-            UNLINK("PlayerModuleTest2.rec");
+            UNLINK("PlayerModule2Test.rec");
         }
 
         void testCorrectMonotonicTemporalOrderAndException() {
             cout << __FILE__ << " " << __LINE__ << endl;
+            UNLINK("PlayerModule2Test.rec");
+            UNLINK("PlayerModule2Test.rec.mem");
+
             // Prepare record file.
             fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
 
@@ -372,13 +387,13 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
             TS_ASSERT(exceptionCaught);
 
-            UNLINK("PlayerModuleTest2.rec");
+            UNLINK("PlayerModule2Test.rec");
         }
 
         void testCorrectMonotonicTemporalOrderAndRewindAndException() {
             cout << __FILE__ << " " << __LINE__ << endl;
             // Prepare record file.
-            fstream fout("PlayerModule2Test2.rec", ios::out | ios::binary | ios::trunc);
+            fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
 
             // Write data with non-monotonic order.
             TimeStamp ts1(4, 5);
@@ -410,7 +425,7 @@ class PlayerModule2Test : public CxxTest::TestSuite {
             fout.flush();
             fout.close();
 
-            const URL u("file://PlayerModule2Test2.rec");
+            const URL u("file://PlayerModule2Test.rec");
 
             // Create Player2 instance.
             const bool NO_AUTO_REWIND = false;
@@ -450,7 +465,7 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
             TS_ASSERT(exceptionCaught);
 
-            UNLINK("PlayerModule2Test2.rec");
+            UNLINK("PlayerModule2Test.rec");
         }
 
         void testCorrectMonotonicTemporalOrderAndAutoRewind() {
@@ -537,6 +552,134 @@ class PlayerModule2Test : public CxxTest::TestSuite {
 
             UNLINK("PlayerModule2Test2.rec");
         }
+
+        void testCorrectMonotonicTemporalOrderAndExceptionFromReverseOrderWithExceptionSharedData() {
+            cout << __FILE__ << " " << __LINE__ << endl;
+            UNLINK("PlayerModule2Test.rec");
+            UNLINK("PlayerModule2Test.rec.mem");
+
+            // Prepare .rec file.
+            {
+                fstream fout("PlayerModule2Test.rec", ios::out | ios::binary | ios::trunc);
+
+                // Write data with non-monotonic order.
+                TimeStamp ts0(5, 6);
+                TimeStamp ts1(4, 5);
+                TimeStamp ts2(3, 4);
+                TimeStamp ts3(2, 3);
+                TimeStamp ts4(1, 2);
+
+                Container c0(ts0);
+                c0.setSampleTimeStamp(ts0);
+                fout << c0;
+
+                Container c1(ts1);
+                c1.setSampleTimeStamp(ts1);
+                fout << c1;
+
+                Container c2(ts2);
+                c2.setSampleTimeStamp(ts2);
+                fout << c2;
+
+                Container c3(ts3);
+                c3.setSampleTimeStamp(ts3);
+                fout << c3;
+
+                Container c4(ts4);
+                c4.setSampleTimeStamp(ts4);
+                fout << c4;
+
+                fout.flush();
+                fout.close();
+            }
+
+            // Prepare .rec.mem file.
+            {
+                fstream fout("PlayerModule2Test.rec.mem", ios::out | ios::binary | ios::trunc);
+
+                // Write data with non-monotonic order.
+                TimeStamp ts0(5, 61);
+                TimeStamp ts1(4, 51);
+                TimeStamp ts2(3, 41);
+                TimeStamp ts3(2, 31);
+                TimeStamp ts4(1, 21);
+
+                odcore::data::SharedData sd;
+                sd.setSize(10);
+                sd.setName("ABC");
+                string data0("5BCDEFGHIJ");
+                string data1("4BCDEFGHIJ");
+                string data2("3BCDEFGHIJ");
+                string data3("2BCDEFGHIJ");
+                string data4("1BCDEFGHIJ");
+
+                Container c0(sd);
+                c0.setSampleTimeStamp(ts0);
+                fout << c0;
+                fout.write(data0.c_str(), data0.size());
+
+                Container c1(sd);
+                c1.setSampleTimeStamp(ts1);
+                fout << c1;
+                fout.write(data1.c_str(), data1.size());
+
+                Container c2(sd);
+                c2.setSampleTimeStamp(ts2);
+                fout << c2;
+                fout.write(data2.c_str(), data2.size());
+
+                Container c3(sd);
+                c3.setSampleTimeStamp(ts3);
+                fout << c3;
+                fout.write(data3.c_str(), data3.size());
+
+                Container c4(sd);
+                c4.setSampleTimeStamp(ts4);
+                fout << c4;
+                fout.write(data4.c_str(), data4.size());
+
+                fout.flush();
+                fout.close();
+            }
+
+            const URL u("file://PlayerModule2Test.rec");
+
+            // Create Player2 instance.
+            const bool NO_AUTO_REWIND = false;
+            Player2 p2(u, NO_AUTO_REWIND);
+
+            TimeStamp before;
+            int64_t counter = 1;
+            while (p2.hasMoreData()) {
+                const Container& c = p2.getNextContainerToBeSent();
+                if (counter > 1) {
+                    TS_ASSERT(p2.getDelay() == 1000001);
+                }
+
+                Thread::usleepFor(p2.getDelay());
+
+                TS_ASSERT((counter * 1000 * 1000 + (counter + 1)) == c.getSampleTimeStamp().toMicroseconds());
+                counter++;
+            }
+            TimeStamp after;
+            cout << "Duration = " << (after - before).toMicroseconds() << endl;
+
+            bool exceptionCaught = false;
+            try {
+                const Container& c55 = p2.getNextContainerToBeSent();
+                (void)c55;
+            }
+            catch(...) {
+                exceptionCaught = true;
+            }
+
+            TS_ASSERT(exceptionCaught);
+
+            UNLINK("PlayerModule2Test.rec");
+            UNLINK("PlayerModule2Test.rec.mem");
+        }
+
+
 
 //        void testLargeFileTwiddle() {
 //            cout << __FILE__ << " " << __LINE__ << endl;
